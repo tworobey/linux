184c318cf596544fad4936013fccfb50fd386d44
tworobey <tworobey@example.com>
2026-05-20 18:49:32 +0000

    restore final solution

--- server/src/json_loader.h
+++ server/src/json_loader.h
@@ -3,9 +3,15 @@
 #include <filesystem>
 
 #include "model.h"
+#include "extra_data.h"
 
 namespace json_loader {
 
-model::Game LoadGame(const std::filesystem::path& json_path);
+struct LoadResult {
+    model::Game game;
+    extra_data::MapExtraData extra_data;
+};
+
+LoadResult LoadGame(const std::filesystem::path& json_path);
 
 }  // namespace json_loader
